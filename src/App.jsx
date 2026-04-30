8748afa6170f4713f7ed659851b94d178a1767dc
ifeMakinde <makindeifeoluwa07@gmail.com>
2026-04-30 12:14:00 +0100

    react init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <section className="text-4xl text-center">
+        {" "}
+        <h1 className="text-red-500"> REFILA</h1>
+      </section>
+    </>
+  );
+}
+
+export default App;
